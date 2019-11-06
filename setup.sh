4fb07706b8eefa6f3b760e5dd1bd1392bf5701f9
mbrother2 <thanhlvtr@gmail.com>
2019-11-06 15:49:23 +0700

    fix replace backup config file

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ GDRIVE_BIN="/usr/sbin/gdrive"
 CRON_BACKUP="/usr/sbin/cron_backup.sh"
 CRON_FILE="/var/spool/cron/root"
 
-change_gdrive_config(){
+# Change backup config file
+change_backup_config(){
     if [ ! -z $2 ]
     then
-        sed -i 's/^$1=.*/$1="$2"//' ${CRON_BACKUP}
+        sed -i "s#^$1=.*#$1=\"$2\"#g" ${CRON_BACKUP}
     fi
 }
 
@@ -35,9 +36,9 @@ read -p " Which directory do you want to upload to Google Drive?(default /backup
 read -p " Which file do you want to get gdrive log?(default /var/log/gdrive.log): " LOG_FILE
 read -p " How many days you want to keep backup on Google Drive?(default 7): " DAY_REMOVE
 
-change_gdrive_config BACKUP_DIR ${BACKUP_DIR}
-change_gdrive_config LOG_FILE ${LOG_FILE}
-change_gdrive_config DAY_REMOVE ${DAY_REMOVE}
+change_backup_config BACKUP_DIR ${BACKUP_DIR}
+change_backup_config LOG_FILE ${LOG_FILE}
+change_backup_config DAY_REMOVE ${DAY_REMOVE}
 echo "PATH=$PATH" >> ${CRON_FILE}
 echo "* * * * * sh /usr/sbin/cron_backup.sh >/dev/null 2>&1" >> ${CRON_FILE}
 systemctl restart crond

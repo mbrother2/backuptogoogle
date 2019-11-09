19ef65e7d0a3d6e6a829f58985d4a7994253c0ce
mbrother2 <thanhlvtr@gmail.com>
2019-11-09 09:26:18 +0700

    write log setup to file & change name log file

--- setup.sh
+++ setup.sh
@@ -4,19 +4,62 @@
 GITHUB_LINK="https://raw.githubusercontent.com/mbrother2/backuptogoogle/master"
 GDRIVE_BIN="/usr/sbin/gdrive"
 CRON_BACKUP="/usr/sbin/cron_backup.sh"
+LOG_FILE="/var/log/backuptogoogle.log"
+
+# Color variables
+BLUE='\e[94m'
+GRAY='\e[90m'
+GREEN='\e[32m'
+PURPLE='\e[35m'
+RED='\e[31m'
+YELLOW='\e[33m'
+REMOVE='\e[0m'
+
+# Change color of words
+change_color(){
+    case $1 in
+          blue) echo -e "${BLUE}$2${REMOVE}";;
+          gray) echo -e "${GRAY}$2${REMOVE}";;
+         green) echo -e "${GREEN}$2${REMOVE}";;
+        purple) echo -e "${PURPLE}$2${REMOVE}";;
+           red) echo -e "${RED}$2${REMOVE}";;
+        yellow) echo -e "${YELLOW}$2${REMOVE}";;
+             *) echo "$2";;
+    esac
+}
+
+# Write log
+show_write_log(){
+    echo "`date "+[ %d/%m/%Y %H:%M:%S ]"` $1" | tee -a ${LOG_FILE}
+}
+
+# Check network
+check_network(){
+    show_write_log "Cheking network..."
+    if ping -c 1 -w 1 raw.githubusercontent.com > /dev/null
+    then
+        show_write_log "Network OK!"
+    else
+        show_write_log "`change_color red [CHECKS][FAIL]` Can not connect to Github file, please check your network"
+        exit 1
+    fi
+}
 
 # Detect OS
 detect_os(){
+    show_write_log "Checking OS..."
     if [ -f /etc/redhat-release ]
     then
         CRON_FILE="/var/spool/cron/root"
+        
     elif [ -f /usr/bin/lsb_release ]
     then
         CRON_FILE="/var/spool/cron/crontabs/root"
     else
-        echo "Sorry! We do not support your OS."
+        show_write_log "Sorry! We do not support your OS."
         exit 1
     fi
+    show_write_log "OS supported!"
 }
 
 # Change backup config file
@@ -29,29 +72,25 @@ change_backup_config(){
 
 # Download file from Github
 download_file(){
-    echo "Downloading gdrive file from github..."
+    show_write_log "Downloading gdrive file from github..."
     curl -o ${GDRIVE_BIN} ${GITHUB_LINK}/gdrive_linux
-    echo ""
-    echo "Downloading script cron file from github..."
+    show_write_log "Downloading script cron file from github..."
     curl -o ${CRON_BACKUP} ${GITHUB_LINK}/cron_backup.sh
     chmod 755 ${GDRIVE_BIN} ${CRON_BACKUP}
 }
 
 # Setup gdrive credential
 setup_credential(){
-    echo ""
-    echo "Setting up gdrive credential..."
+    show_write_log "Setting up gdrive credential..."
     gdrive about
 }
 
 # Set up cron backup
 setup_cron(){
-    echo ""
-    echo "Setting up cron backup..."
+    show_write_log "Setting up cron backup..."
     read -p " Which directory do you want to upload to Google Drive?(default /backup): " BACKUP_DIR
-    read -p " Which file do you want to get gdrive log?(default /var/log/gdrive.log): " LOG_FILE
     read -p " How many days you want to keep backup on Google Drive?(default 7): " DAY_REMOVE
-
+    
     change_backup_config BACKUP_DIR ${BACKUP_DIR}
     change_backup_config LOG_FILE ${LOG_FILE}
     change_backup_config DAY_REMOVE ${DAY_REMOVE}
@@ -62,15 +101,15 @@ setup_cron(){
 
 show_info(){
     echo ""
-    echo "SUCESSFUL! Your information:"
-    echo "---"
-    echo "Backup dir : ${BACKUP_DIR:-/backup}"
-    echo "Log file   : ${LOG_FILE:-/var/log/gdrive.log}"
-    echo "Keep backup: ${DAY_REMOVE:-7} days"
-    echo "---"
-    echo "Gdrive bin file : ${GDRIVE_BIN}"
-    echo "Cron backup file: ${CRON_BACKUP}"
-    echo "Cron backup     : 0 0 * * * sh ${CRON_BACKUP} >/dev/null 2>&1"
+    show_write_log "SUCESSFUL! Your information:"
+    show_write_log "---"
+    show_write_log "Backup dir : ${BACKUP_DIR:-/backup}"
+    show_write_log "Log file   : ${LOG_FILE}"
+    show_write_log "Keep backup: ${DAY_REMOVE:-7} days"
+    show_write_log "---"
+    show_write_log "Gdrive bin file : ${GDRIVE_BIN}"
+    show_write_log "Cron backup file: ${CRON_BACKUP}"
+    show_write_log "Cron backup     : 0 0 * * * sh ${CRON_BACKUP} >/dev/null 2>&1"
     echo ""
     echo " If you get trouble when use backuptogoogle please go to following URLs:"
     echo " https://backuptogoogle.com"
@@ -78,6 +117,7 @@ show_info(){
 }
 
 # Main functions
+check_network
 detect_os
 download_file
 setup_credential

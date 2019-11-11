82dc073fde761520068beccb902d918b04bf6d69
mbrother2 <thanhlvtr@gmail.com>
2019-11-11 15:36:04 +0700

    check some informations before run

--- cron_backup.sh
+++ cron_backup.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 # Setup variables
-BACKUP_DIR="/backup"
-LOG_FILE="/var/log/backuptogoogle.log"
-DAY_REMOVE="7"
+BUTGG_CONF="${HOME}/.gdrive/butgg.conf"
+DF_BACKUP_DIR="${HOME}/backup"
+DF_LOG_FILE="${HOME}/.gdrive/butgg.log"
+DF_DAY_REMOVE="7"
 FIRST_OPTION=$1
 
 # Date variables
 TODAY=`date +"%d_%m_%Y"`
-OLD_BACKUP_DAY=`date +%d_%m_%Y -d "-${DAY_REMOVE} day"`
 
 # Color variables
-BLUE='\e[94m'
-GRAY='\e[90m'
 GREEN='\e[32m'
-PURPLE='\e[35m'
 RED='\e[31m'
 YELLOW='\e[33m'
 REMOVE='\e[0m'
@@ -22,10 +19,7 @@ REMOVE='\e[0m'
 # Change color of words
 change_color(){
     case $1 in
-          blue) echo -e "${BLUE}$2${REMOVE}";;
-          gray) echo -e "${GRAY}$2${REMOVE}";;
          green) echo -e "${GREEN}$2${REMOVE}";;
-        purple) echo -e "${PURPLE}$2${REMOVE}";;
            red) echo -e "${RED}$2${REMOVE}";;
         yellow) echo -e "${YELLOW}$2${REMOVE}";;
              *) echo "$2";;
@@ -52,24 +46,58 @@ check_file_type(){
     fi
 }
 
+# Write config
+check_config(){
+    if [ "$3" == "" ]
+    then
+        VAR=$1
+        eval "$VAR"="$2"
+        [[ $1 == LOG_FILE ]] && show_write_log "---"
+        show_write_log "`change_color yellow [WARNING]` $1 does not exist. Use default config"
+        [[ -f ${BUTGG_CONF} ]] && sed -i "/^$1/d" ${BUTGG_CONF}
+        echo "$1=$2" >> ${BUTGG_CONF}
+    else
+        VAR=$1
+        eval "$VAR"="$3"
+        [[ $1 == LOG_FILE ]] && show_write_log "---"
+    fi
+}
+
+# Get config
+get_config(){
+    if [ ! -f ${BUTGG_CONF} ]
+    then
+        check_config LOG_FILE ${DF_LOG_FILE}
+        check_config BACKUP_DIR ${DF_BACKUP_DIR}
+        check_config DAY_REMOVE ${DF_DAY_REMOVE}
+    else
+        LOG_FILE=`cat ${BUTGG_CONF} | grep "^LOG_FILE"   | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config LOG_FILE ${DF_LOG_FILE} ${LOG_FILE}
+        BACKUP_DIR=`cat ${BUTGG_CONF} | grep "^BACKUP_DIR" | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config BACKUP_DIR ${DF_BACKUP_DIR} ${BACKUP_DIR}           
+        DAY_REMOVE=`cat ${BUTGG_CONF} | grep "^DAY_REMOVE" | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config DAY_REMOVE ${DF_DAY_REMOVE} ${DAY_REMOVE} 
+    fi
+}
+
 # Check infomations before upload to Google Drive
 check_info(){
-    show_write_log "---"
     if [ ! -d "${BACKUP_DIR}" ]
     then       
         show_write_log "`change_color red [CHECKS][FAIL]` Directory ${BACKUP_DIR} does not exist. Exit"
         exit 1
     fi
-    if [ ! -f /root/.gdrive/token_v2.json ]
+    if [ ! -f ${HOME}/.gdrive/token_v2.json ]
     then
-        show_write_log "`change_color red [CHECKS][FAIL]` File /root/.gdrive/token_v2.json does not exist. Exit"
+        show_write_log "`change_color red [CHECKS][FAIL]` File ${HOME}/.gdrive/token_v2.json does not exist. Exit"
         show_write_log "Please run command: 'gdrive about' to create your Google token for gdrive"
         exit 1
     else
         echo "\n" | gdrive list >/dev/null
         if [ $? -ne 0 ]
         then
-            show_write_log "`change_color red [CHECKS][FAIL]` File /root/.gdrive/token_v2.json exists but can not verify Google token for gdrive. Exit"
+            echo ""
+            show_write_log "`change_color red [CHECKS][FAIL]` File ${HOME}/.gdrive/token_v2.json exists but can not verify Google token for gdrive. Exit"
             show_write_log "Please run command: 'gdrive about' to recreate your Google token for gdrive"
             exit 1
         fi
@@ -98,6 +126,7 @@ run_upload(){
     else
         :
     fi
+    BACKUP_DIR=`realpath ${BACKUP_DIR}`
     for i in $(ls -1 ${BACKUP_DIR})
     do
         check_file_type "${BACKUP_DIR}/$i"            
@@ -116,6 +145,7 @@ run_upload(){
 }
 
 remove_old_dir(){
+    OLD_BACKUP_DAY=`date +%d_%m_%Y -d "-${DAY_REMOVE} day"`
     OLD_BACKUP_ID=`gdrive list -m 100000 --name-width 0 | grep "${OLD_BACKUP_DAY}" | awk '{print $1}'`
     if [ "${OLD_BACKUP_ID}" != "" ]
     then
@@ -133,6 +163,7 @@ remove_old_dir(){
 }
 
 # Main functions
+get_config
 check_info
 run_upload
 remove_old_dir

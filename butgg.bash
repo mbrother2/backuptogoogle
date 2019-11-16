7177d67c3f92b6991936175141f1f3410c186bc4
mbrother2 <thanhlvtr@gmail.com>
2019-11-16 10:58:41 +0700

    use bash on Linux system

---
+++ butgg.bash
@@ -0,0 +1,430 @@
+#!/bin/sh
+
+# Set variables
+GITHUB_LINK="https://raw.githubusercontent.com/mbrother2/backuptogoogle/master"
+GO_FILE="go1.12.5.linux-amd64"
+BUTGG_CONF="${HOME}/.gdrive/butgg.conf"
+DF_BACKUP_DIR="${HOME}/backup"
+DF_LOG_FILE="${HOME}/.gdrive/butgg.log"
+DF_DAY_REMOVE="7"
+GDRIVE_BIN="${HOME}/bin/gdrive"
+GDRIVE_TOKEN="${HOME}/.gdrive/token_v2.json"
+CRON_BACKUP="${HOME}/bin/cron_backup.bash"
+SETUP_FILE="${HOME}/bin/butgg.bash"
+CRON_TEMP="${HOME}/.gdrive/old_cron"
+SECOND_OPTION=$2
+
+# Date variables
+TODAY=`date +"%d_%m_%Y"`
+
+# Color variables
+GREEN='\e[32m'
+RED='\e[31m'
+YELLOW='\e[33m'
+REMOVE='\e[0m'
+
+# Change color of words
+change_color(){
+    case $1 in
+         green) echo -e "${GREEN}$2${REMOVE}";;
+           red) echo -e "${RED}$2${REMOVE}";;
+        yellow) echo -e "${YELLOW}$2${REMOVE}";;
+             *) echo "$2";;
+    esac
+}
+
+# Check MD5 of downloaded file
+check_md5sum(){
+    curl -o $2 ${GITHUB_LINK}/$1
+    ORIGIN_MD5=`curl -s ${GITHUB_LINK}/MD5SUM | grep $1 | awk '{print $1}'`
+    LOCAL_MD5=`md5sum $2 | awk '{print $1}'`
+    if [ "${ORIGIN_MD5}" == "${LOCAL_MD5}" ]
+    then
+        show_write_log "Check md5sum for file $1 successful"
+    else
+        show_write_log "`change_color red [CHECKS][FAIL]` Can not verify md5 for file $1. Exit!"
+        exit 1
+    fi
+}
+
+# Check log file
+check_log_file(){    
+    if [ ! -f ${BUTGG_CONF} ]
+    then
+        LOG_FILE=${DF_LOG_FILE}        
+    else
+        LOG_FILE=`cat ${BUTGG_CONF} | grep "^LOG_FILE" | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        if [ "${LOG_FILE}" == "" ]
+        then
+            LOG_FILE=${DF_LOG_FILE}
+        fi
+    fi
+    create_dir .gdrive
+    create_dir bin
+}
+
+# Write log
+show_write_log(){
+    echo "`date "+[ %d/%m/%Y %H:%M:%S ]"` $1" | tee -a ${LOG_FILE}
+}
+
+# Create necessary directory
+create_dir(){
+    if [ ! -d ${HOME}/$1 ]
+    then
+        mkdir -p ${HOME}/$1
+        if [ ! -d ${HOME}/$1 ]
+        then
+            echo "Can not create directory ${HOME}/$1. Exit"
+            exit 1
+        else
+            if [ "$1" == ".gdrive" ]
+            then
+                show_write_log "---"
+                show_write_log "Creating necessary directory..."
+            fi
+            show_write_log "Create directory ${HOME}/$1 successful"
+        fi
+    else
+        if [ "$1" == ".gdrive" ]
+        then
+            show_write_log "---"
+            show_write_log "Creating necessary directory..."
+        fi
+        show_write_log "Directory ${HOME}/$1 existed. Skip"
+    fi
+    echo 1 >> ${HOME}/$1/test.txt
+    if [ $? -ne 0 ]
+    then
+        echo "Can not write to ${HOME}/$1. Exit"
+        exit 1
+    else
+        show_write_log "Check write to ${HOME}/$1 successful"
+    fi
+    rm -f ${HOME}/$1/test.txt
+}
+
+check_package(){
+    which $1
+    if [ $? -ne 0 ]
+    then
+        show_write_log "Command $1 not found. Trying to install $1..."
+        sleep 3
+        ${INSTALL_CM} install -y $1
+        which $1
+        if [ $? -ne 0 ]
+        then
+            show_write_log "Can not install $1 package. Please install $1 manually."
+            exit 1
+        fi
+    fi
+    show_write_log "Package $1 is installed"
+}
+
+# Check network
+check_network(){
+    show_write_log "Cheking network..."
+    if ping -c 1 raw.githubusercontent.com > /dev/null
+    then
+        show_write_log "Connect Github successful"
+    else
+        show_write_log "`change_color red [CHECKS][FAIL]` Can not connect to Github file, please check your network. Exit"
+        exit 1
+    fi
+    if ping -c 1 dl.google.com > /dev/null
+    then
+        show_write_log "Connect Google successful"
+    else
+        show_write_log "`change_color red [CHECKS][FAIL]` Can not connect to Github file, please check your network. Exit"
+        exit 1
+    fi
+}
+
+# Detect OS
+detect_os(){
+    show_write_log "Checking OS..."
+    if [ -f /etc/os-release ]
+    then
+        OS=`cat /etc/os-release | grep "^NAME=" | cut -d'"' -f2 | awk '{print $1}'`
+        if [ "${OS}" == "CentOS" ]
+        then
+            INSTALL_CM="yum"
+        elif [ "${OS}" == "Ubuntu" ]
+        then
+            INSTALL_CM="apt"
+        elif [ "${OS}" == "openSUSE" ]
+        then
+            INSTALL_CM="zypper"
+        else
+            show_write_log "Sorry! We do not support your OS. Exit"
+            exit 1
+        fi
+    else
+        show_write_log "Sorry! We do not support your OS. Exit"
+        exit 1
+    fi
+    show_write_log "OS supported"
+    show_write_log "Checking necessary package..."
+    check_package curl
+    check_package git
+}
+
+# Download file from Github
+download_file(){
+    show_write_log "Downloading script cron file from github..."
+    check_md5sum cron_backup.bash "${CRON_BACKUP}"
+    show_write_log "Downloading setup file from github..."
+    check_md5sum butgg.bash "${SETUP_FILE}"
+    chmod 755 ${CRON_BACKUP} ${SETUP_FILE}
+}
+
+# Build GDRIVE_BIN
+build_gdrive(){
+    cd $HOME/bin
+    show_write_log "Downloading go from Google..."
+    curl -o ${GO_FILE}.tar.gz https://dl.google.com/go/${GO_FILE}.tar.gz
+    show_write_log "Extracting go lang..."
+    tar -xf ${GO_FILE}.tar.gz
+    show_write_log "Cloning gdrive project from Github..."
+    rm -rf gdrive
+    git clone https://github.com/gdrive-org/gdrive.git
+    show_write_log "Build your own gdrive!"
+    echo "Please go to URL to create your own Google credential:"
+    echo "https://github.com/mbrother2/backuptogoogle/wiki/Create-own-Google-credential-step-by-step"
+    read -p " Your Google API client_id: " gg_client_id
+    read -p " Your Google API client_secret: " gg_client_secret
+    sed -i "s#^const ClientId =.*#const ClientId = \"${gg_client_id}\"#g" $HOME/bin/gdrive/handlers_drive.go
+    sed -i "s#^const ClientSecret =.*#const ClientSecret = \"${gg_client_secret}\"#g" $HOME/bin/gdrive/handlers_drive.go
+    show_write_log "Building gdrive..."
+    cd $HOME/bin/gdrive
+    $HOME/bin/go/bin/go get github.com/prasmussen/gdrive
+    $HOME/bin/go/bin/go build -ldflags '-w -s'
+    if [ $? -ne 0 ]
+    then
+        show_write_log "`change_color red [ERROR]` Can not build gdrive. Exit"
+        exit 1
+    else
+        show_write_log "Build gdrive successful. Gdrive bin locate here ${GDRIVE_BIN} "
+    fi
+    mv $HOME/bin/gdrive/gdrive $HOME/bin/gdrive.bin
+    chmod 755 $HOME/bin/gdrive
+    rm -f $HOME/bin/${GO_FILE}.tar.gz
+    rm -rf $HOME/bin/go
+    rm -rf $HOME/bin/gdrive
+    mv $HOME/bin/gdrive.bin $HOME/bin/gdrive
+}
+
+# Setup gdrive credential
+setup_credential(){
+    show_write_log "Setting up gdrive credential..."
+    if [ "${SECOND_OPTION}" == "credential" ]
+    then
+        if [ -f ${GDRIVE_TOKEN} ]
+        then
+            rm -f ${GDRIVE_TOKEN}
+        fi
+    fi
+    ${GDRIVE_BIN} about
+    if [ $? -ne 0 ]
+    then
+        show_write_log "`change_color yellow [WARNING]` Can not create gdrive credential. Please run \"${GDRIVE_BIN} about\" to create it after"
+        sleep 3
+    else
+        show_write_log "Setup gdrive credential successful"
+    fi
+}
+
+# Set up config file
+setup_config(){
+    show_write_log "Setting up config file..."
+    read -p " Which directory do you want to upload to Google Drive?(default ${DF_BACKUP_DIR}): " BACKUP_DIR
+    read -p " How many days you want to keep backup on Google Drive?(default ${DF_DAY_REMOVE}): " DAY_REMOVE    
+    if [ -z "${BACKUP_DIR}" ]
+    then
+        BACKUP_DIR="${DF_BACKUP_DIR}"
+    fi
+    if [ -z "${DAY_REMOVE}" ]
+    then
+        DAY_REMOVE="${DF_DAY_REMOVE}"
+    fi
+    echo "LOG_FILE=${LOG_FILE}" > ${BUTGG_CONF}
+    echo "BACKUP_DIR=${BACKUP_DIR}" >> ${BUTGG_CONF}
+    echo "DAY_REMOVE=${DAY_REMOVE}" >> ${BUTGG_CONF}
+    if [ $? -ne 0 ]
+    then
+        show_write_log "`change_color red [ERROR]` Can not write config to file ${BUTGG_CONF}. Please check permission of this file. Exit"
+        exit 1
+    else
+        if [ ! -d ${BACKUP_DIR} ]
+        then
+            show_write_log "`change_color yellow [WARNING]` Directory ${BACKUP_DIR} does not exist! Ensure you will be create it after."
+            sleep 3
+        fi
+        show_write_log "Setup config file successful"
+    fi       
+}
+
+# Set up cron backup
+setup_cron(){
+    show_write_log "Setting up cron backup..."
+    crontab -l > ${CRON_TEMP}
+    CHECK_CRON=`cat ${CRON_TEMP} | grep -c "cron_backup.sh"`
+    if [ ${CHECK_CRON} -eq 0 ]
+    then
+        echo "PATH=$PATH" >> ${CRON_TEMP}
+        echo "0 0 * * * sh ${CRON_BACKUP} >/dev/null 2>&1" >> ${CRON_TEMP}
+        crontab ${CRON_TEMP}
+        if [ $? -ne 0 ]
+        then
+            show_write_log "Can not setup cronjob to backup! Please check again"
+            SHOW_CRON="`change_color yellow [WARNING]` Can not setup cronjob to backup"
+        else
+            show_write_log "Setup cronjob to backup successful"
+            SHOW_CRON="0 0 * * * bash ${CRON_BACKUP} >/dev/null 2>&1"
+        fi
+    else
+        show_write_log "Cron backup existed. Skip"
+        SHOW_CRON=`cat ${CRON_TEMP} | grep "cron_backup.bash"`
+    fi
+    rm -f  ${CRON_TEMP}
+}
+
+# Show information
+show_info(){
+    echo ""
+    if [ "${SECOND_OPTION}" == config ]
+    then
+        show_write_log "+-----"
+        show_write_log "| SUCESSFUL! Your information:"
+        show_write_log "| Backup dir      : ${BACKUP_DIR}"
+        show_write_log "| Keep backup     : ${DAY_REMOVE} days"
+        show_write_log "| Config file     : ${BUTGG_CONF}"
+        show_write_log "+-----"
+    else
+        show_write_log "+-----"
+        show_write_log "| SUCESSFUL! Your information:"
+        show_write_log "| Backup dir      : ${BACKUP_DIR}"
+        show_write_log "| Config file     : ${BUTGG_CONF}"
+        show_write_log "| Log file        : ${LOG_FILE}"
+        show_write_log "| Keep backup     : ${DAY_REMOVE} days"
+        show_write_log "| butgg.bash file   : ${SETUP_FILE}"
+        show_write_log "| Cron backup file: ${CRON_BACKUP}"
+        show_write_log "| Gdrive bin file : ${GDRIVE_BIN}"
+        show_write_log "| Cron backup     : ${SHOW_CRON}"
+        show_write_log "| Google token    : ${GDRIVE_TOKEN}"
+        show_write_log "+-----"
+
+        echo ""
+        echo " If you get trouble when use butgg.bash please report here:"
+        echo " https://github.com/mbrother2/backuptogoogle/issues"
+    fi
+}
+
+_setup(){
+    check_log_file
+    if [ -z "${SECOND_OPTION}" ]
+    then
+        check_network
+        detect_os
+        download_file
+        build_gdrive
+        setup_credential
+        setup_config
+        setup_cron
+        show_info
+    else
+        case ${SECOND_OPTION} in
+            config)
+                setup_config
+                show_info
+                ;;
+            credential)
+                setup_credential
+                ;;
+            only-build)
+                check_network
+                detect_os
+                build_gdrive
+                ;;
+            no-build)
+                check_network
+                detect_os
+                download_file
+                setup_credential
+                setup_config
+                setup_cron
+                show_info
+                ;;
+            no-update)
+                check_network
+                detect_os
+                build_gdrive
+                setup_credential
+                setup_config
+                setup_cron
+                show_info
+                ;;
+            *)
+                show_write_log "No such command: ${SECOND_OPTION}. Please use butgg.bash --help"
+                ;;
+        esac
+    fi
+}
+
+_update(){
+    check_log_file
+    check_network
+    detect_os
+    download_file
+}
+
+_uninstall(){
+    check_log_file
+    show_write_log "Removing all butgg.bash scripts..."
+    rm -f ${GDRIVE_BIN} ${CRON_BACKUP} ${SETUP_FILE}
+    if [ $? -ne 0 ]
+    then
+        show_write_log "Can not remove all butgg.bash scripts. Please check permission of these files"
+    else
+        show_write_log "Remove all butgg.bash scripts successful"
+    fi
+    read -p " Do you want remove ${HOME}/.gdrive directory?(y/n) " REMOVE_GDRIVE_DIR
+    if [ "${REMOVE_GDRIVE_DIR}" == "y" ] || [ "${REMOVE_GDRIVE_DIR}" == "Y" ]
+    then
+        rm -rf ${HOME}/.gdrive
+        if [ $? -ne 0 ]
+        then
+            show_write_log "Can not remove directory ${HOME}/.gdrive. Please check permission of this directory"
+        else
+            echo "Remove directory ${HOME}/.gdrive successful"
+        fi
+    else
+        show_write_log "Skip remove ${HOME}/.gdrive directory"
+    fi
+}
+
+_help(){
+    echo "butgg.bash - Backup to Google Drive solution"
+    echo ""
+    echo "Usage: butgg.bash [options] [command]"
+    echo ""
+    echo "Options:"
+    echo "  --help       show this help message and exit"
+    echo "  --setup      setup or reset all scripts & config file"
+    echo "    config     only setup config"
+    echo "    credential only setup credential"
+    echo "    only-build only build gdrive bin"
+    echo "    no-build   setup butgg without build gdrive"
+    echo "    no-update  setup butgg without update script"
+    echo "  --update     update to latest version"
+    echo "  --uninstall  remove all butgg scripts and .gdrive directory"
+}
+
+# Main functions
+case $1 in
+    --help)      _help ;;
+    --setup)     _setup ;;
+    --update)    _update ;;
+    --uninstall) _uninstall ;;
+    *)           echo "No such option: $1. Please use butgg.bash --help" ;;
+esac
---
+++ cron_backup.bash
@@ -0,0 +1,196 @@
+#!/bin/bash
+
+# Setup variables
+BUTGG_CONF="${HOME}/.gdrive/butgg.conf"
+GDRIVE_BIN="${HOME}/bin/gdrive"
+DF_BACKUP_DIR="${HOME}/backup"
+DF_LOG_FILE="${HOME}/.gdrive/butgg.log"
+DF_DAY_REMOVE="7"
+FIRST_OPTION=$1
+
+# Date variables
+TODAY=`date +"%d_%m_%Y"`
+
+# Color variables
+GREEN='\e[32m'
+RED='\e[31m'
+YELLOW='\e[33m'
+REMOVE='\e[0m'
+
+# Change color of words
+change_color(){
+    case $1 in
+         green) echo -e "${GREEN}$2${REMOVE}";;
+           red) echo -e "${RED}$2${REMOVE}";;
+        yellow) echo -e "${YELLOW}$2${REMOVE}";;
+             *) echo "$2";;
+    esac
+}
+
+# Show processing and write log
+show_write_log(){
+    if [ "${FIRST_OPTION}" == "-v" ]
+    then
+        echo `date "+[ %d/%m/%Y %H:%M:%S ]"` $1
+    fi
+    echo `date "+[ %d/%m/%Y %H:%M:%S ]"` $1 >> ${LOG_FILE}
+}
+
+# Check file type
+check_file_type(){
+    if [ -d $1 ]
+    then
+        FILE_TYPE="directory"
+    elif [ -f $1 ]
+    then
+        FILE_TYPE="file"
+    else
+        show_write_log "`change_color red [CHECKS][FAIL]` Can not detect file type for $1. Exit"
+        exit 1
+    fi
+}
+
+# Detect OS
+detect_os(){
+    show_write_log "Checking OS..."
+    if [ -f /etc/os-release ]
+    then
+        OS=`cat /etc/os-release | grep "^NAME=" | cut -d'"' -f2 | awk '{print $1}'`
+        show_write_log "OS supported"
+    else
+        show_write_log "Sorry! We do not support your OS. Exit"
+        exit 1
+    fi
+}
+
+# Write config
+check_config(){
+    if [ "$3" == "" ]
+    then
+        VAR=$1
+        eval "$VAR"="$2"
+        if [ $1 == LOG_FILE ]
+        then
+            show_write_log "---"
+        fi
+        show_write_log "`change_color yellow [WARNING]` $1 does not exist. Use default config"
+        if [ -f ${BUTGG_CONF} ]
+        then
+            sed -i "/^$1/d" ${BUTGG_CONF}
+        fi
+        echo "$1=$2" >> ${BUTGG_CONF}
+    else
+        VAR=$1
+        eval "$VAR"="$3"
+        if [ $1 == LOG_FILE ]
+        then
+            show_write_log "---"
+        fi
+    fi
+}
+
+# Get config
+get_config(){
+    if [ ! -f ${BUTGG_CONF} ]
+    then
+        check_config LOG_FILE ${DF_LOG_FILE}
+        check_config BACKUP_DIR ${DF_BACKUP_DIR}
+        check_config DAY_REMOVE ${DF_DAY_REMOVE}
+    else
+        LOG_FILE=`cat ${BUTGG_CONF} | grep "^LOG_FILE"   | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config LOG_FILE ${DF_LOG_FILE} ${LOG_FILE}
+        BACKUP_DIR=`cat ${BUTGG_CONF} | grep "^BACKUP_DIR" | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config BACKUP_DIR ${DF_BACKUP_DIR} ${BACKUP_DIR}           
+        DAY_REMOVE=`cat ${BUTGG_CONF} | grep "^DAY_REMOVE" | cut -d"=" -f2 | sed 's/"//g' | sed "s/'//g"`
+        check_config DAY_REMOVE ${DF_DAY_REMOVE} ${DAY_REMOVE} 
+    fi
+}
+
+# Check infomations before upload to Google Drive
+check_info(){
+    if [ ! -d "${BACKUP_DIR}" ]
+    then       
+        show_write_log "`change_color red [CHECKS][FAIL]` Directory ${BACKUP_DIR} does not exist. Exit"
+        exit 1
+    fi
+    if [ ! -f ${HOME}/.gdrive/token_v2.json ]
+    then
+        show_write_log "`change_color red [CHECKS][FAIL]` File ${HOME}/.gdrive/token_v2.json does not exist. Exit"
+        show_write_log "Please run command: '${GDRIVE_BIN} about' to create your Google token for gdrive"
+        exit 1
+    else
+        echo "\n" | ${GDRIVE_BIN} list >/dev/null
+        if [ $? -ne 0 ]
+        then
+            echo ""
+            show_write_log "`change_color red [CHECKS][FAIL]` File ${HOME}/.gdrive/token_v2.json exists but can not verify Google token for gdrive. Exit"
+            show_write_log "Please run command: '${GDRIVE_BIN} about' to recreate your Google token for gdrive"
+            exit 1
+        fi
+    fi
+}
+
+# Run upload to Google Drive
+run_upload(){
+    show_write_log "Start upload to Google Drive..."
+    CHECK_BACKUP_DIR=`${GDRIVE_BIN} list -m 100000 --name-width 0 | grep -c "${TODAY}"`
+    if [ ${CHECK_BACKUP_DIR} -eq 0 ]
+    then
+        show_write_log "Directory ${TODAY} does not exist. Creating..."
+        ID_DIR=`${GDRIVE_BIN} mkdir ${TODAY} | awk '{print $2}'`
+    else
+        show_write_log "Directory ${TODAY} existed. Skipping..."
+        ID_DIR=`${GDRIVE_BIN} list -m 100000 --name-width 0 | grep "${TODAY}" | head -1 | awk '{print $1}'`
+    fi
+    if [ ${#ID_DIR} -ne 33 ]
+    then
+        show_write_log "`change_color red [CREATE][FAIL]` Can not create directory ${TODAY}"
+        exit 1
+    elif [ ${CHECK_BACKUP_DIR} -eq 0 ]
+    then
+        show_write_log "`change_color green [CREATE]` Created directory ${TODAY} with ID ${ID_DIR}"
+    else
+        :
+    fi
+    BACKUP_DIR=`realpath ${BACKUP_DIR}`
+    for i in $(ls -1 ${BACKUP_DIR})
+    do
+        check_file_type "${BACKUP_DIR}/$i"            
+        show_write_log "Uploading ${FILE_TYPE} ${BACKUP_DIR}/$i to directory ${TODAY}..."                
+        UPLOAD_FILE=`${GDRIVE_BIN} upload -p ${ID_DIR} --recursive ${BACKUP_DIR}/$i`
+        if [[ "${UPLOAD_FILE}" == *"Error"* ]] || [[ "${UPLOAD_FILE}" == *"Fail"* ]]
+        then
+            show_write_log "`change_color red [UPLOAD][FAIL]` Can not upload backup file! ${UPLOAD_FILE}"
+            show_write_log "Something wrong!!! Exit."
+            exit
+        else
+            show_write_log "`change_color green [UPLOAD]` Uploaded ${FILE_TYPE} ${BACKUP_DIR}/$i to directory ${TODAY}"
+        fi
+    done
+    show_write_log "Finish! All files and directories in ${BACKUP_DIR} are uploaded to Google Drive in directory ${TODAY}"
+}
+
+remove_old_dir(){
+    OLD_BACKUP_DAY=`date +%d_%m_%Y -d "-${DAY_REMOVE} day"`
+    OLD_BACKUP_ID=`${GDRIVE_BIN} list -m 100000 --name-width 0 | grep "${OLD_BACKUP_DAY}" | awk '{print $1}'`
+    if [ "${OLD_BACKUP_ID}" != "" ]
+    then
+        ${GDRIVE_BIN} delete -r ${OLD_BACKUP_ID}
+        OLD_BACKUP_ID=`${GDRIVE_BIN} list -m 100000 --name-width 0 | grep "${OLD_BACKUP_DAY}" | awk '{print $1}'`
+        if [[ "${OLD_BACKUP_ID}" == "" ]]
+        then
+            show_write_log "`change_color green [REMOVE]` Removed directory ${OLD_BACKUP_DAY}"
+        else
+            show_write_log "`change_color red [REMOVE][FAIL]` Directory ${OLD_BACKUP_DAY} exists but can not remove!"
+        fi
+    else
+        show_write_log "Directory ${OLD_BACKUP_DAY} does not exist. Nothing need remove!"
+    fi
+}
+
+# Main functions
+get_config
+detect_os
+check_info
+run_upload
+remove_old_dir

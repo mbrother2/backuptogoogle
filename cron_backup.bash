5b7cf88d660cdf4dbac334c81689f924f91bdd69
mbrother2 <thanhlvtr@gmail.com>
2019-12-05 17:11:36 +0700

    fix read tmp file

--- cron_backup.bash
+++ cron_backup.bash
@@ -348,7 +348,7 @@ run_sync(){
     fi
     show_write_log "Start sync to Google Drive..."
     ${GDRIVE_BIN} sync upload --keep-local --delete-extraneous "${BACKUP_DIR}" "${GDRIVE_ID}" | while IFS= read -r line; do printf '[ %s ] %s\n' "$(date '+%d/%m/%Y %H:%M:%S')" "$line" | tee -a ${HOME}/${RANDOM_STRING}.tmp; done
-    CHECK_SYNC=`cat ${RANDOM_STRING}.tmp | grep -c "Root directory is not empty"`
+    CHECK_SYNC=`cat ${HOME}/${RANDOM_STRING}.tmp | grep -c "Root directory is not empty"`
     if [ ${CHECK_SYNC} -eq 0 ]
     then
         cat ${HOME}/${RANDOM_STRING}.tmp >> ${LOG_FILE}
